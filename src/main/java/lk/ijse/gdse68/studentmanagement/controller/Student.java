b77dc23e04c28924695508bc4183ef2568c7e806
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-07-14 09:49:48 +0530

    add init

--- src/main/java/lk/ijse/gdse68/studentmanagement/controller/Student.java
+++ src/main/java/lk/ijse/gdse68/studentmanagement/controller/Student.java
@@ -2,6 +2,8 @@ package lk.ijse.gdse68.studentmanagement.controller;
 
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,9 +12,26 @@ import lk.ijse.gdse68.studentmanagement.dto.StudentDTO;
 import lk.ijse.gdse68.studentmanagement.util.Util;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/student")
 public class Student extends HttpServlet {
+    Connection connection;
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        try{
+            var dbClass = getServletContext().getInitParameter("db-class");
+            var dbUrl = getServletContext().getInitParameter("db-url");
+            var dbUsername = getServletContext().getInitParameter("db-username");
+            var dbPassword = getServletContext().getInitParameter("db-password");
+            Class.forName(dbClass);
+            DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -20,17 +39,20 @@ public class Student extends HttpServlet {
         if (req.getContentType() == null || !req.getContentType().toLowerCase().startsWith("application/json")) {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
+        try (var writer = resp.getWriter()){
 
-        //object binding of the json
-        Jsonb jsonb = JsonbBuilder.create();
-        StudentDTO student = jsonb.fromJson(req.getReader(), StudentDTO.class);
-        student.setId(Util.idGenerate());
-        System.out.println("Student ID: " + student.getId());
-        System.out.println("Student Name: " + student.getName());
-        System.out.println("Student Email: " + student.getEmail());
-        System.out.println("Student Level: " + student.getLevel());
-        resp.setContentType("application/json");
-        jsonb.toJson(student, resp.getWriter());
+            //object binding of the json
+            Jsonb jsonb = JsonbBuilder.create();
+            StudentDTO student = jsonb.fromJson(req.getReader(), StudentDTO.class);
+            student.setId(Util.idGenerate());
+            System.out.println("Student ID: " + student.getId());
+            System.out.println("Student Name: " + student.getName());
+            System.out.println("Student Email: " + student.getEmail());
+            System.out.println("Student Level: " + student.getLevel());
+            resp.setContentType("application/json");
+            jsonb.toJson(student, resp.getWriter());
+
+        }
     }
 
     @Override
